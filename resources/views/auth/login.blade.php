ad9846416e10ff5e4785e4bec59fd324009f8179
bilash0283 <bilash0283@gmail.com>
2025-06-21 15:20:43 +0600

    login page modify

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,6 @@
 @include('layouts.header')
 <x-guest-layout>
     <!-- Session Status -->
-    <!-- <x-auth-session-status class="mb-4" :status="session('status')" /> -->
-
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
