84d16948c7d049c22db08315c692389d62277854
bilash0283 <bilash0283@gmail.com>
2025-06-25 17:41:10 +0600

    hader page almost donw

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -37,9 +37,11 @@
                 </a>
             </div>
             <div class="md:w-1/2">
-                <img src="https://source.unsplash.com/600x400/?technology" alt="Hero Image"
+                <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Hero Image"
                     class="rounded-lg shadow-lg" />
             </div>
         </div>
     </section>
+
+
 
