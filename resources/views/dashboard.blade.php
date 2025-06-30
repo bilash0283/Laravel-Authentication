8a583461169c1084ff187cb237d84d3b5d23535f
bilash0283 <bilash0283@gmail.com>
2025-06-30 12:25:32 +0600

    dashboard modify

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,15 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div class="flex col-span-4 gap-3 bg-slate-400 p-4">
-                        <h1 class="text-[20px] font-bold text-white">This is Heading</h1>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
     </div>
 </x-app-layout>
 
