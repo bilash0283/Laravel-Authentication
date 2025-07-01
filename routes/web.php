0567c0d92d8633e7465bc350877ea729f60ff658
bilash0283 <bilash0283@gmail.com>
2025-07-01 12:49:27 +0600

    category page modify

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/category-create',[Category::class,'create'])->name('category_create');
+
 });
 
 require __DIR__.'/auth.php';
