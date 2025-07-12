d2fea4b9c67864ca64f1e91426766e51b0acb0f5
bilash0283 <bilash0283@gmail.com>
2025-07-12 18:09:38 +0600

    create page modify

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -65,7 +65,7 @@
             </div>
             <div class="max-w-sm my-2">
                 <label for="status" class="block text-sm font-medium mb-2">Status</label>
-                <select class="py-3 px-4 pe-9 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none bg-white dark:border-transparent dark:text-neutral-400 ">
+                <select class="py-3 px-4 pe-9 block w-full bg-white border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:border-transparent dark:text-neutral-400 ">
                     <option selected="">Open this select menu</option>
                     <option value="1">Active</option>
                     <option value="2">Inactive</option>
