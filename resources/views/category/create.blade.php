492f9f9f03a6098c216f787878cbb2ca0d65b9f7
bilash0283 <bilash0283@gmail.com>
2025-07-13 17:24:58 +0600

    create page modify

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -51,31 +51,34 @@
     <main class="flex-1 p-4 sm:p-6">
       <h1 class="text-xl font-bold ">Add Category Page</h1>
         <div class="w-full my-6">
-            <div class="max-w-sm my-2">
-                <label for="input-label" class="block text-sm font-medium mb-2">Name</label>
-                <input type="email" id="input-label" class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter Name">  
-            </div>
-            <div class="max-w-sm my-2">
-                <label for="description" class="block text-sm font-medium mb-2">Description</label>
-                <input type="text" id="description" class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter Description">  
-            </div>
-            <div class="max-w-sm my-2">
-                <label for="description" class="block text-sm font-medium mb-2">Image</label>
-                <input type="file" id="description" class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter Description">  
-            </div>
-            <div class="max-w-sm my-2">
-                <label for="status" class="block text-sm font-medium mb-2">Status</label>
-                <select class="py-3 px-4 pe-9 block w-full bg-white border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:border-transparent dark:text-neutral-400 ">
-                    <option selected="">Open this select menu</option>
-                    <option value="1">Active</option>
-                    <option value="2">Inactive</option>
-                </select>
-            </div>
-            <div class="max-w-sm my-2">
-                <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-yellow-100 text-green-800 hover:bg-yellow-200 focus:outline-hidden focus:bg-yellow-200 disabled:opacity-50 disabled:pointer-events-none dark:text-yellow-500">
-                    Submit Info
-                </button>
-            </div>
+            <form action="" method="POST" enctype="multipart/form-data">
+              @csrf
+              <div class="max-w-sm my-2">
+                  <label for="input-label" class="block text-sm font-medium mb-2">Name</label>
+                  <input type="email" id="input-label" class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter Name">  
+              </div>
+              <div class="max-w-sm my-2">
+                  <label for="description" class="block text-sm font-medium mb-2">Description</label>
+                  <input type="text" id="description" class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter Description">  
+              </div>
+              <div class="max-w-sm my-2">
+                  <label for="description" class="block text-sm font-medium mb-2">Image</label>
+                  <input type="file" id="description" class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter Description">  
+              </div>
+              <div class="max-w-sm my-2">
+                  <label for="status" class="block text-sm font-medium mb-2">Status</label>
+                  <select class="py-3 px-4 pe-9 block w-full bg-white border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:border-transparent dark:text-neutral-400 ">
+                      <option selected="">Open this select menu</option>
+                      <option value="1">Active</option>
+                      <option value="2">Inactive</option>
+                  </select>
+              </div>
+              <div class="max-w-sm my-2">
+                  <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-yellow-100 text-green-800 hover:bg-yellow-200 focus:outline-hidden focus:bg-yellow-200 disabled:opacity-50 disabled:pointer-events-none dark:text-yellow-500">
+                      Submit Info
+                  </button>
+              </div>
+            </form>
         </div>
     </main>
 
@@ -90,6 +93,9 @@
 
 </body>
 </html>
+
+
+
 
 
 
