00466caa914c31d62c70e8ffda424bb4c1a0a9af
bilash0283 <bilash0283@gmail.com>
2025-07-15 12:39:46 +0600

    category model modify

--- resources/views/category/view_cate.blade.php
+++ resources/views/category/view_cate.blade.php
@@ -29,7 +29,9 @@
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">sdfsdf</td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">fsdfsd</td>
                                     <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-                                        <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-hidden focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Delete</button>
+                                        <a href="#" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-hidden focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Edit</a>
+
+                                        <a href="#" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-red-600 hover:red-blue-800 focus:outline-hidden focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Delete</a>
                                     </td>
                                 </tr>
                         </table>
