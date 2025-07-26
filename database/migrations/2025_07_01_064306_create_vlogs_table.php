7cbb9c4c564589094ae9828c1cf60dff01f28a80
bilash0283 <bilash0283@gmail.com>
2025-07-26 17:24:16 +0600

    vlog page modify

--- database/migrations/2025_07_01_064306_create_vlogs_table.php
+++ database/migrations/2025_07_01_064306_create_vlogs_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('vlogs', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('category');
+            $table->string('image')->nullable();
+            $table->integer('status');
             $table->timestamps();
         });
     }
