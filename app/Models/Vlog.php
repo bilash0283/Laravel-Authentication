72ca53fce0ec957f5165d9f55608274dbe710164
bilash0283 <bilash0283@gmail.com>
2025-07-27 18:15:00 +0600

    today end

--- app/Models/Vlog.php
+++ app/Models/Vlog.php
@@ -13,38 +13,41 @@ class Vlog extends Model
         return view('vlog.create',['categoryes' => $category]);
     }
 
-   public function vlog_store(Request $request)
-    {
-        $validated = $request->validate([
+    public function vlog_store(Request $request){
+       $valided = $request->validator([
             'name' => "required",
             'description' => "required",
             'category' => "required",
-            'image' => "nullable|mimes:jpg,jpeg,png",
+            'image' => "mimes:jpg,jpeg,png",
             'status' => "required"
         ]);
 
         $imageFull_name = '';
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-            $image_name = time() . '.' . $image->getClientOriginalExtension();
-            $image->move(public_path('images/vlog_image'), $image_name);
-            $imageFull_name = 'images/vlog_image/' . $image_name;
+        if(!empty($request->hasFile('image'))){
+            $image_name = time().'.'.$request-file('image');
+            $imageFull_name = "images/vlog_image/".$image_name;
+            $request->image->move(public_path('images/vlog_image'), $image_name);
         }
 
-        $vlog = new Vlog();
-        $vlog->name = $validated['name'];
-        $vlog->description = $validated['description'];
-        $vlog->category = $validated['category'];
+
+       $vlog = new Vlog() ;
+
+        $vlog->name = $valided['name'];
+        $vlog->description = $valided['description'];
+        $vlog->category = $valided['category'];
         $vlog->image = $imageFull_name;
-        $vlog->status = $validated['status'];
+        $vlog->status = $valided['status'];
+
         $vlog->save();
 
-        return redirect()->route('vlog_manage')->with('success', 'Vlog saved successfully.');
+        return redirect()->route('vlog_manage')->with('success','Vlog Save Successfull');
+
+
     }
 
     public function vlog_manege(){
-        $vlogs = Vlog::with('category')->orderBy('id','desc')->get();
-    return view('vlog.manage_vlog', ['vlogs' => $vlogs]);
+        $vlog = Vlog::orderBy('id','desc')->get();
+        return view('vlog.manage_vlog',['vlogs' => $vlog]);
     }
 
 
