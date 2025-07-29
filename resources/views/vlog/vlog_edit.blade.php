693c571adbff2557e911bbb3f1415776f41cd107
bilash0283 <bilash0283@gmail.com>
2025-07-29 18:41:39 +0600

    vlog page modify

--- resources/views/vlog/vlog_edit.blade.php
+++ resources/views/vlog/vlog_edit.blade.php
@@ -27,6 +27,7 @@
                   </select>
                 </div>
               <div class="max-w-sm my-2">
+                  <img src="{{ asset($vlog->image) }}" alt="" width="40px" height="40px">
                   <label for="description" class="block text-sm font-medium mb-2">Image</label>
                   <input type="file" name="image" id="description" class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter Description">  
               </div>
