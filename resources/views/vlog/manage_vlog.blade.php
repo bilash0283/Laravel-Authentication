bf636db5450ba7c6f6b4a1bc137f76e478f89f44
bilash0283 <bilash0283@gmail.com>
2025-07-29 17:36:24 +0600

    vlog modle modify

--- resources/views/vlog/manage_vlog.blade.php
+++ resources/views/vlog/manage_vlog.blade.php
@@ -37,9 +37,9 @@
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-{{ $vlog->status == 1 ? 'green' : 'red' }}-800">{{ $vlog->status == 1 ? "Active" : "Inactive" }}</td>
                                     <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-                                        <!-- <a href="{{ route('category_edit',[$vlog->id])}}" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-hidden focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Edit</a>
+                                        <a href="{{ route('vlog_edit',[$vlog->id])}}" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-hidden focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Edit</a>
 
-                                        <a href="{{ route('category_delete',[$vlog->id])}}" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-red-600 hover:red-blue-800 focus:outline-hidden focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none" onclick="confirm('Are You Sure to Delete it ?')">Delete</a> -->
+                                        <a href="{{ route('vlog_delete',[$vlog->id])}}" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-red-600 hover:red-blue-800 focus:outline-hidden focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none" onclick="confirm('Are You Sure to Delete it ?')">Delete</a>
                                     </td>
                                 </tr>
                             @endforeach
